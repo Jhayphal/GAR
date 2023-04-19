3abaefdfe39445c4b9dd8529f8fa071855170663
Oleg Nazarenko <olegn@devart.com>
2023-04-19 20:56:41 +0300

    Добавил возможность потокового чтения XML-фалов из ZIP-архива без предварительной распаковки последнего.

---
+++ GarDataLoader/Services/ZipWalker.cs
@@ -0,0 +1,19 @@
+﻿using Ionic.Zip;
+
+namespace GarDataLoader.Services;
+
+public static class ZipWalkerService
+{
+  public static IEnumerable<Stream> GetFiles(string fileName, string fileMask)
+  {
+    using var zip = ZipFile.Read(fileName);
+    foreach (var entryFileName in zip.EntryFileNames)
+    {
+      if (entryFileName.Contains(fileMask))
+      {
+        System.Diagnostics.Debug.WriteLine(entryFileName);
+        yield return zip[entryFileName].OpenReader();
+      }
+    }
+  }
+}
--- GarDataView/ViewModels/MainWindowViewModel.cs
+++ GarDataView/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Reactive;
 using System.IO;
+using GarDataLoader.Services;
 using GarModels;
 using Microsoft.Data.SqlClient;
 using GarModels.Services;
@@ -13,27 +14,39 @@ namespace GarDataView.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-  private const string ConnectionString = @"Data Source=NJNOUTE;Database=Empty;User ID=sa;Password=123456;TrustServerCertificate=True";
-  
   private readonly IGarDataService<IAddressObject> addressObjectsDataService = Locator.Current.GetService<IGarDataService<IAddressObject>>();
   private readonly IGarDataService<IItemRelation> itemRelationsDataService = Locator.Current.GetService<IGarDataService<IItemRelation>>();
   private readonly IGarDataService<IHouse> housesDataService = Locator.Current.GetService<IGarDataService<IHouse>>();
 
-  private string folderPath = @"C:\Users\Jhayphal\Downloads\93";
+  private string archivePath = @"C:\Users\Jhayphal\Downloads\gar_xml.zip";
+
+  private string connectionString =
+    @"Data Source=NJNOUTE;Database=Empty;User ID=sa;Password=123456;TrustServerCertificate=True";
 
   public MainWindowViewModel()
   {
-    var canExecute = this.WhenAnyValue(x => x.FolderPath, s => !string.IsNullOrWhiteSpace(s) && Directory.Exists(s));
+    var canExecute = this.WhenAnyValue(
+      vm => vm.ConnectionString, 
+      vm => vm.ArchivePath, 
+      (connection, archive) => !string.IsNullOrWhiteSpace(archive) 
+                               && !string.IsNullOrWhiteSpace(connection) 
+                               && File.Exists(archive));
 
     InsertAddressObjects = ReactiveCommand.CreateFromTask(InsertObjects, canExecute);
     InsertItemRelations = ReactiveCommand.CreateFromTask(InsertRelations, canExecute);
     InsertHouseObjects = ReactiveCommand.CreateFromTask(InsertHouses, canExecute);
   }
 
-  public string FolderPath
+  public string ConnectionString
   {
-    get => folderPath;
-    set => this.RaiseAndSetIfChanged(ref folderPath, value);
+    get => connectionString;
+    set => this.RaiseAndSetIfChanged(ref connectionString, value);
+  }
+
+  public string ArchivePath
+  {
+    get => archivePath;
+    set => this.RaiseAndSetIfChanged(ref archivePath, value);
   }
 
   public ReactiveCommand<Unit, Unit> InsertAddressObjects { get; }
@@ -44,39 +57,39 @@ public class MainWindowViewModel : ViewModelBase
 
   private async Task InsertObjects()
   {
-    foreach (var file in GetFilesByMask("AS_ADDR_OBJ_2*.XML"))
+    foreach (var stream in ZipWalkerService.GetFiles(ArchivePath, "/AS_ADDR_OBJ_2"))
     {
-      using var stream = new StreamReader(file);
+      await using var _ = stream.ConfigureAwait(false);
+      using var reader = new StreamReader(stream);
       var connection = new SqlConnection(ConnectionString);
-      await using var _ = connection.ConfigureAwait(false);
+      await using var __ = connection.ConfigureAwait(false);
       await connection.OpenAsync();
-      await addressObjectsDataService.InsertRecordsFromAsync(connection, "ADDRESS_OBJECTS", stream);
+      await addressObjectsDataService.InsertRecordsFromAsync(connection, "ADDRESS_OBJECTS", reader);
     }
   }
 
   private async Task InsertRelations()
   {
-    foreach (var file in GetFilesByMask("AS_ADM_HIERARCHY_*.XML"))
+    foreach (var stream in ZipWalkerService.GetFiles(ArchivePath, "/AS_ADM_HIERARCHY_"))
     {
-      using var stream = new StreamReader(file);
+      await using var _ = stream.ConfigureAwait(false);
+      using var reader = new StreamReader(stream);
       var connection = new SqlConnection(ConnectionString);
-      await using var _ = connection.ConfigureAwait(false);
+      await using var __ = connection.ConfigureAwait(false);
       await connection.OpenAsync();
-      await itemRelationsDataService.InsertRecordsFromAsync(connection, "ADM_HIERARCHY", stream);
+      await itemRelationsDataService.InsertRecordsFromAsync(connection, "ADM_HIERARCHY", reader);
     }
   }
   
   private async Task InsertHouses()
   {
-    foreach (var file in GetFilesByMask("AS_HOUSES_2*.XML"))
+    foreach (var stream in ZipWalkerService.GetFiles(ArchivePath, "/AS_HOUSES_2"))
     {
-      using var stream = new StreamReader(file);
+      using var reader = new StreamReader(stream);
       var connection = new SqlConnection(ConnectionString);
-      await using var _ = connection.ConfigureAwait(false);
+      await using var __ = connection.ConfigureAwait(false);
       await connection.OpenAsync();
-      await housesDataService.InsertRecordsFromAsync(connection, "HOUSES", stream);
+      await housesDataService.InsertRecordsFromAsync(connection, "HOUSES", reader);
     }
   }
-
-  private IEnumerable<string> GetFilesByMask(string mask) => Directory.GetFiles(FolderPath, mask, SearchOption.AllDirectories);
 }
